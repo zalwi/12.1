4f0abfa8cb970321e4663839199629bc7471f7b0
Kamil Zalwert <kamilz89@gmail.com>
2020-05-18 14:08:22 +0200

    12.1 - Wykorzystano biblioteke do walidacji ciągów znaków jako liczby, zmiana ArrayList na czysty interfejs List oraz drobne błędy

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import logic.UniqueValues;
 
 import javax.xml.crypto.Data;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         //Wczytaj wartości z pliku
-        ArrayList<String> values = DataReader.readValuesFromFile("data.dat");
+        List<String> values = DataReader.readValuesFromFile("data.dat");
         //Utwórz kontener z mapą
-        UniqueValues uniqueValues = new UniqueValues(values);
+        UniqueValues uniqueValues = new UniqueValues((ArrayList<String>)values);
         //Drukuj wszystkie unikalne wartości oraz ich ilość wystąpień
         uniqueValues.printUniqueValuesAndCounters(false);
         System.out.println("-- Tylko liczby --");
--- src/logic/UniqueValues.java
+++ src/logic/UniqueValues.java
@@ -1,53 +1,43 @@
 package logic;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.*;
 
 public class UniqueValues {
 
-    Map<String, Integer> UniqueValuesAndCounters;
+    Map<String, Integer> uniqueValuesAndCounters;
 
     public UniqueValues(ArrayList<String> values) {
-        UniqueValuesAndCounters = new TreeMap<String, Integer>();
+        uniqueValuesAndCounters = new TreeMap<String, Integer>();
         fillMap(values);
     }
 
-    private void fillMap(ArrayList<String> values) {
+    private void fillMap(List<String> values) {
         for (String val : values) {
             addUniqueValeuOrIncreaseCounter(val);
         }
     }
 
     private void addUniqueValeuOrIncreaseCounter(String value) {
-        if (UniqueValuesAndCounters.containsKey(value)) {
-            int counter = UniqueValuesAndCounters.get(value);
-            UniqueValuesAndCounters.put(value, ++counter);
+        if (uniqueValuesAndCounters.containsKey(value)) {
+            int counter = uniqueValuesAndCounters.get(value);
+            uniqueValuesAndCounters.put(value, ++counter);
         } else {
-            UniqueValuesAndCounters.put(value, 1);
+            uniqueValuesAndCounters.put(value, 1);
         }
     }
 
     public void printUniqueValuesAndCounters(boolean onlyNumbers) {
-        Set<Map.Entry<String, Integer>> entries = UniqueValuesAndCounters.entrySet();
+        Set<Map.Entry<String, Integer>> entries = uniqueValuesAndCounters.entrySet();
         for (Map.Entry entry : entries) {
             if (onlyNumbers) {
-                if (isNumeric(entry.getKey().toString())) { // drukuj tylko liczby
+                if (NumberUtils.isParsable(entry.getKey().toString())) { // drukuj tylko liczby
                     System.out.println("'" + entry.getKey() + "'\t\t– liczba wystąpień:\t" + entry.getValue());
                 }
             } else {
                 System.out.println("'" + entry.getKey() + "'\t\t– liczba wystąpień:\t" + entry.getValue());
             }
         }
-    }
-
-    private boolean isNumeric(String strNum) {
-        if (strNum == null) {
-            return false;
-        }
-        try {
-            double d = Double.parseDouble(strNum);
-        } catch (NumberFormatException nfe) {
-            return false;
-        }
-        return true;
     }
 }
